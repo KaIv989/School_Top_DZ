b5fc643a61aecc27a9215fa8bbfd01998a96cea2
HOME_Ivan <vanyuha3005@gmail.com>
2023-01-12 22:53:01 +0300

    поправки по присвоению нового значения приватному атрибуту

--- DZ_19_01_23/DZ_19_01_23.py
+++ DZ_19_01_23/DZ_19_01_23.py
@@ -15,19 +15,22 @@ class Car:
         self.__price = price
 
     def change(self, arg, value):
-        self.__dict__[arg] = value
-
+        if arg == 'price':
+            self.__price = value
+        else:
+            self.__dict__[arg] = value
     def __str__(self):
+        
         return f'{self.__dict__}'
 
 
 kia = Car('Cerato', 2010, 'kia', '1.6', 'red', 650000)
-print(kia.name, kia.brand, kia.color)
+print(kia)
 
+
+# меняем атрибуты через метод change
 kia.change('name', 'civic')
 kia.change('brand', 'honda')
 kia.change('color', 'black')
-kia.change('__price', 1)
-kia.__price = 550000
-print(kia.name, kia.brand, kia.color, kia.__price)
+kia.change('price', 550000)
 print(kia)
