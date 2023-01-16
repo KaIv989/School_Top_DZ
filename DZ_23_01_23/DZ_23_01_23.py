8a87ee7762b24c252d6a88ab488c9d56f25320d1
KaIv989_Work <kaiv989@gmail.com>
2023-01-16 12:06:27 +0300

    Зд по декораторам финал

--- DZ_23_01_23/DZ_23_01_23.py
+++ DZ_23_01_23/DZ_23_01_23.py
@@ -12,23 +12,20 @@ def decorator(fun):
 
 @decorator
 def prime_numbers(x = 0, y = 1000):
-    # Меняем местами если указан не верный диапазон чисел
+    # Меняем местами если указан не верный порядок чисел
     if x > y:
         x, y = y, x
-    # Если диапазон до 3, то сразу возвращаем результат (исключая 0)
-    if y < 4:
-        if x < 1:
-            x = 1
-        return list(range(x, y+1))
+    if y < 3:
+        return [i for i in range(2, y+1)]
     # Через генератор создаем список сразу без четных
-    arr = [i for i in range(x, y+1) if i % 2 or i == 2]
-    count = 2
+    arr = [i for i in range(2, y+1) if i % 2 or i == 2]
+    count = 1
 
-    while count <= len(arr)//3:  # оптимальное условия для больших и маленьких диапазонов.
+    while count - 1 <= len(arr)//10:  # оптимизация для ускорения работы программы.
         value = arr[count]
-        arr = list(filter(lambda e: e == value or e % value, arr))
+        arr = list(filter(lambda e: (e == value or e % value) and e > x, arr))
         count += 1
     return arr
 
 
-print(prime_numbers(0, 10000))
+print(prime_numbers(0, 100000))
